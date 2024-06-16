b786544c6e6e25459f5bafe6209f5255ff09acaf
Yaw Owusu Kessie <yawowusukessie@gmail.com>
2024-06-16 00:32:21 +0000

    Added web.dot and web.svg, modified web.tf; deleted output.tf

--- web.tf
+++ web.tf
@@ -5,13 +5,13 @@ provider "aws" {
 
 # Module: VPC Basic
 module "vpc_basic" {
-  source        = "github.com/kessiey/tf_vpc.git?ref=v0.0.1" # Ensure this path is correct
+  source        = "github.com/kessiey/tf_vpc.git?ref=v0.0.1"
   name          = "web"
   cidr          = "10.0.0.0/16"
   public_subnet = "10.0.1.0/24"
 }
 
-# Resource: AWS Instance for Web
+# Resource: AWS EC2 Instance for Web
 resource "aws_instance" "web" {
   ami                         = var.ami[var.region]
   instance_type               = var.instance_type
@@ -19,44 +19,51 @@ resource "aws_instance" "web" {
   subnet_id                   = module.vpc_basic.public_subnet_id
   associate_public_ip_address = true
   user_data                   = file("files/web_bootstrap.sh")
-
+  
   vpc_security_group_ids = [
     aws_security_group.web_inbound_sg.id,
   ]
-
+  
   tags = {
     Name = "web-${format("%03d", count.index + 1)}"
   }
-
+  
   count = length(var.instance_ips)
 }
 
-# Resource: AWS ELB for Web
+# Resource: AWS Elastic Load Balancer (ELB)
 resource "aws_elb" "web" {
   name            = "web-elb"
   subnets         = [module.vpc_basic.public_subnet_id]
   security_groups = [aws_security_group.web_inbound_sg.id]
-
+  
   listener {
     instance_port     = 80
-    instance_protocol = "HTTP"
+    instance_protocol = "http"
     lb_port           = 80
-    lb_protocol       = "HTTP"
+    lb_protocol       = "http"
   }
-
-  instances = aws_instance.web.*.id
+  
+  instances = aws_instance.web[*].id
 }
 
-# Resource: AWS Security Group for Web Inbound
+# Resource: AWS Security Groups
 resource "aws_security_group" "web_inbound_sg" {
-  name        = "web-inbound-sg"
-  description = "Security group for web server"
+  name        = "web_inbound"
+  description = "Allow HTTP and ICMP from Anywhere"
   vpc_id      = module.vpc_basic.vpc_id
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -68,27 +75,36 @@ resource "aws_security_group" "web_inbound_sg" {
   }
 }
 
+resource "aws_security_group" "web_host_sg" {
+  name        = "web_host"
+  description = "Allow SSH and HTTP to web hosts"
+  vpc_id      = module.vpc_basic.vpc_id
 
-# # Resource for AWS Security Group for Web Host
-# resource "aws_security_group" "web_host_sg" {
-#   name        = "web-host-sg"
-#   description = "Allow outbound web traffic"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [module.vpc_basic.cidr]
+  }
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "web-host-sg"
-#   }
-# }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
